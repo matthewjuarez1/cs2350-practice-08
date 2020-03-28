45c485e609feeca2650d072ef8c518dabe4490e3
Matthew Juarez <matthewjuarez1@mail.weber.edu>
2020-03-28 17:13:13 -0600

    day 1

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -5,3 +5,12 @@ require.context('../stylesheets/', true, /\.(css|scss)$/i)
 
 // JavaScript
 //TODO
+import { movies } from './movies'
+let featured_movie = document.querySelector('.featured')
+for (let m of movies){
+  let movie_thumb = document.getElementById('m' + m.id)
+  movie_thumb.innerHTML = 
+  <img src="${m.poster}">
+
+  
+}
